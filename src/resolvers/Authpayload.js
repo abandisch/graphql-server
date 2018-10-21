c6855d74aaf514bb95e918e0d6e45a2e3ea9b100
Alex Bandisch <alexander.bandisch@gmail.com>
2018-10-21 10:54:36 +1000

    add query, mutation and authpayload resolvers

---
+++ src/resolvers/Authpayload.js
@@ -0,0 +1,5 @@
+/* Authpayload Resolvers */
+
+const user = (root, args, context, info) => context.db.query.user({ where: { id: root.user.id } }, info)
+
+module.exports = { user }
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,44 @@
+/* Mutation Resolvers */
+
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const { getUserId } = require('../utils')
+const { APP_JWT_SECRET } = require('../config')
+
+const signWithJwt = (item) => jwt.sign(item, APP_JWT_SECRET)
+
+async function signup (root, args, context, info) {
+  const password = await bcrypt.hash(args.password, 10)
+
+  const user = await context.db.mutation.createUser({
+    data: { ...args, password }
+  })
+
+  const token = signWithJwt({ userId: user.id })
+
+  return {
+    token,
+    user
+  }
+}
+
+async function login (root, args, context, info) {
+  const user = await context.db.query.user({ where: { email: args.email } }, '{ id password }')
+  if (!user) throw new Error('User not found')
+
+  const valid = await bcrypt.compare(args.password, user.password)
+  if (!valid) throw new Error('Invalid password')
+
+  const token = signWithJwt({ userId: user.id })
+
+  return {
+    token,
+    user
+  }
+}
+
+module.exports = {
+  signup,
+  login,
+  post,
+}
