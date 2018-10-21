01ed9f73c95598d22e817ae91eab988d573f0a63
Alex Bandisch <alexander.bandisch@gmail.com>
2018-10-21 10:51:53 +1000

    add utils to extract user id from jwt

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken')
+const { APP_JWT_SECRET } = require('./config')
+
+const getUserId = context => {
+  const Authorization = context.request.get('Authorization')
+  if (!Authorization) throw new Error ('Not authenticated')
+
+  const token = Authorization.replace('Bearer ', '')
+  const { userId } = jwt.verify(token, APP_JWT_SECRET)
+  return userId
+}
+
+module.export  = { getUserId }
