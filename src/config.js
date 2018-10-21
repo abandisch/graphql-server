c00046dd7a29fd9d3519763e31557645613ba7ac
Alex Bandisch <alexander.bandisch@gmail.com>
2018-10-21 11:32:58 +1000

    add APP_JWT_SECRET to config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 'use strict'
 
+exports.APP_JWT_SECRET = process.env.APP_JWT_SECRET
 exports.PRISMA_JWT_SECRET = process.env.PRISMA_JWT_SECRET
 exports.PRISMA_ENDPOINT = process.env.PRISMA_ENDPOINT
 exports.PORT = process.env.PORT || 4000
